c084a38203e7b988d3d2faad4d71fad00a5339ca
Thiago Vieira <thiago.skrull@gmail.com>
2025-06-09 14:35:59 -0300

    Correção na inicialização pygame
    Consertei :)

--- assets.py
+++ assets.py
@@ -1,23 +1,34 @@
 import pygame
 from pygame import mixer
-pygame.display.set_caption("Juninho's Fighter")
-mixer.init()
 
-# Sons
-pygame.mixer.music.load("assets/audio/WhatsApp-Audio-2025-05-29-at-15.05.08.mp3")
-pygame.mixer.music.set_volume(0.1)
-sword_fx = pygame.mixer.Sound("assets/audio/sword.wav")
-sword_fx.set_volume(0.1)
-magic_fx = pygame.mixer.Sound("assets/audio/magic.wav")
-magic_fx.set_volume(0.25)
+def load_assets():
+    mixer.init()
 
-# Imagens
-background_img = pygame.image.load("assets/imagens/Background/background1.jpeg").convert_alpha()
-victory_img = pygame.image.load("assets/imagens/icons/victory.png").convert_alpha()
+    # Sons
+    pygame.mixer.music.load("assets/audio/WhatsApp-Audio-2025-05-29-at-15.05.08.mp3")
+    pygame.mixer.music.set_volume(0.1)
+    sword_fx = pygame.mixer.Sound("assets/audio/sword.wav")
+    sword_fx.set_volume(0.1)
+    magic_fx = pygame.mixer.Sound("assets/audio/magic.wav")
+    magic_fx.set_volume(0.25)
 
-warrior_sheet = pygame.image.load("assets/imagens/warrior/sprites/warrior.png").convert_alpha()
-wizard_sheet = pygame.image.load("assets/imagens/wizard/sprites/wizard.png").convert_alpha()
+    # Imagens (só agora, com display iniciado)
+    background_img = pygame.image.load("assets/imagens/Background/background1.jpeg").convert_alpha()
+    victory_img = pygame.image.load("assets/imagens/icons/victory.png").convert_alpha()
+    warrior_sheet = pygame.image.load("assets/imagens/warrior/sprites/warrior.png").convert_alpha()
+    wizard_sheet = pygame.image.load("assets/imagens/wizard/sprites/wizard.png").convert_alpha()
 
-# Fontes
-count_font = pygame.font.Font("assets/font/turok.ttf", 80)
-score_font = pygame.font.Font("assets/font/turok.ttf", 80)
+    # Fontes
+    count_font = pygame.font.Font("assets/font/turok.ttf", 80)
+    score_font = pygame.font.Font("assets/font/turok.ttf", 80)
+
+    return {
+        "background_img": background_img,
+        "victory_img": victory_img,
+        "warrior_sheet": warrior_sheet,
+        "wizard_sheet": wizard_sheet,
+        "sword_fx": sword_fx,
+        "magic_fx": magic_fx,
+        "count_font": count_font,
+        "score_font": score_font,
+    }
--- main.py
+++ main.py
@@ -1,23 +1,13 @@
 import pygame
 from config import SCREEN_HEIGHT, SCREEN_WIDTH
 from game import Game
-from assets import background_img, victory_img, warrior_sheet, wizard_sheet, sword_fx, magic_fx, count_font, score_font
+from assets import load_assets
+
 pygame.init()
-pygame.display.set_caption("Juninho's Fighter")
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Juninho's Fighter")
 
+assets = load_assets()
 
-assets = {
-    "background_img": background_img,
-    "victory_img": victory_img,
-    "warrior_sheet": warrior_sheet,
-    "wizard_sheet": wizard_sheet,
-    "sword_fx": sword_fx,
-    "magic_fx": magic_fx,
-    "count_font": count_font,
-    "score_font": score_font,
-}
-
-from game import Game
-game = Game(screen, assets)
+game =Game(screen, assets)
 game.run()
